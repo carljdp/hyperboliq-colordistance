9a54487c7a7cf6c88a73b69e0a14c0c7d8cd65fb
Carl J du Preez <carljdp@gmail.com>
2020-11-19 10:40:07 +0200

    normalize input image

--- main.js
+++ main.js
@@ -2,8 +2,11 @@ const path = require('path')
 const Jimp = require('jimp')
 
 const inputImageRelativePath = path.join('testData')
-const inputImageFilename = path.join('input.jpg')
+const inputImageFilename = 'input.jpg'
 const inputImageUri = path.join(__dirname, inputImageRelativePath, inputImageFilename) 
+
+const desiredPartsInWidth = 20
+const desiredPartsInHeight = 20
 
 async function getImageFromDisk(fileName) {
   try {
@@ -20,11 +23,14 @@ async function main() {
   console.log('\n[MOSAIC] Strating ..')
 
   // get image & dimensions
-  let inputImage = await getImageFromDisk(inputImageUri)
-  console.log(`[MOSAIC] Input Image Dimensions: ${inputImage.bitmap.width}w x ${inputImage.bitmap.height}h`)
+  let inputImageOriginal = await getImageFromDisk(inputImageUri)
+  console.log(`[MOSAIC] Input Image Dimensions (original):   ${inputImageOriginal.bitmap.width}w x ${inputImageOriginal.bitmap.height}h`)
 
   // resize or crop to an easy to use size
-
+  const normalizedInputImageWidth = (Math.ceil(inputImageOriginal.bitmap.width / desiredPartsInWidth) * desiredPartsInWidth)
+  const normalizedInputImageHeight = (Math.ceil(inputImageOriginal.bitmap.height / desiredPartsInHeight) * desiredPartsInHeight)
+  const inputImageNormalized = inputImageOriginal.cover(normalizedInputImageWidth, normalizedInputImageHeight)
+  console.log(`[MOSAIC] Input Image Dimensions (normalized): ${inputImageNormalized.bitmap.width}w x ${inputImageNormalized.bitmap.height}h`)
 
   // divide `input.jpg` into a 20x20 grid of _parts_
 
